370b9932910284a9b88a29e137ddf00c37330b80
ArtemidaGre <99325255+ArtemidaGre@users.noreply.github.com>
2022-10-19 21:25:53 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+class OnStart
+{
+    static void Main()
+    {
+        Log_in();
+        Console.WriteLine("to finish program print any key");
+        Console.ReadKey();
+    }
+    static void Log_in()
+    {
+        string login, password;
+        bool whilet, wand = true;
+        while (true & wand)
+        {
+            Console.Write("enter login\n>");
+            login = Console.ReadLine();
+            if (login == "OS") { OS.System.main(); };
+            Console.Write("enter password\n>");
+            password = Console.ReadLine();
+            if (login == "box" & password == "8246")
+            {
+               Console.Write("Hi, ArtaWWod!\nCommands:\n1) OS\n2) restart\n3) test funk\n4) clear\n5) end\n>>>");
+                string command;
+                whilet = true;
+                while (whilet)
+                {
+                    command = Console.ReadLine();
+                    if (command == "OS" | command == "os" | command == "1") { OS.System.main(); whilet = false; }
+                    else if (command == "restart" | command == "2") { whilet = false; }
+                    else if (command == "test funk" | command == "3") { Console.WriteLine("none"); whilet = false; }
+                    else if (command == "clear" | command == "4") { Console.Clear(); }
+                    else if (command == "end" | command == "5") { whilet = false; wand = false; }
+                    else { Console.Write("Commands:\n1) OS\n2) restart\n3) test funk\n4) clear\n5) end\n>>>"); }
+
+                }
+            }
+            else if (login == "guest" & password == "guest")
+            {
+                Console.WriteLine("Hi, guest, now you can start only Box OS :)\nNow I start Box OS");
+                OS.System.main();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ ModulesV1.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * main file for OS module
+*/
+
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Drawing;
+using System.Net;
+using Windows.System.UserProfile;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace OS
+{
+    class System
+    {
+        public static void main()
+        {
+            Console.WriteLine("starting OS");
+            Startup();
+        }
+        private static void Startup()
+        {
+            string command;
+            bool OS_work = true;
+            Console.WriteLine("Starting modules...");
+            Thread.Sleep(3854);
+            Console.WriteLine("Starting virus...");
+            Thread.Sleep(1282);
+            Console.Write("OS started without errors\nvirus was downloaded $)\nto get help print help\n");
+            while (true & OS_work)
+            {
+                Console.Write(">>>");
+                command = Console.ReadLine();
+                if (command == "help") { Help(); }
+                else if (command == "help help") { Help("help"); }
+                else if (command == "help game") { Help("game"); }
+                else if (command == "help delete") { Help("delete"); }
+                else if (command == "help clear") { Help("help"); }
+                else if (command == "help href") { Help("href"); }
+                else if (command == "clear") { Console.Clear(); }
+                else if (command == "end") { OS_work = false; }
+                else if (command == "delete")
+                {
+                    string path;
+                    Console.Write("print path to file\n>>>");
+                    path = Console.ReadLine();
+                    FileInfo fileInf = new FileInfo(path);
+                    if (fileInf.Exists)
+                    {
+                        fileInf.Delete();
+                        Console.WriteLine("delete succes");
+                    }
+                }
+                else if (command == "game")
+                {
+                    Process mYproces = new Process();
+                    mYproces.StartInfo.FileName = "g.exe";
+                    mYproces.Start();
+                    mYproces.WaitForExit();
+                }
+                else if (command == "href")
+                {
+                    Console.Write(">");
+                    string href = Console.ReadLine();
+                    Process.Start("cmd", "/C start" + " http://" + href);
+                }
+                else if (command == "git")
+                {
+                    Process.Start("cmd", "/C start https://github.com/ArtemidaGre");
+                }
+            }
+        }
+        private static void Help(string To_help = "main")
+        {
+            List<string> Commands = new List<string> { "help", "delete", "game", "clear", "end", "href" };
+            if (To_help == "main")
+            {
+                Console.WriteLine("Command list:");
+                foreach (string item in Commands)
+                    Console.WriteLine(item);
+                Console.WriteLine("example of help command >>> help <name of command>");
+            }
+            else if (To_help == "help") { Console.WriteLine("this command will help you understand commands in the command list :)"); }
+            else if (To_help == "delete") { Console.WriteLine("this command will delete files on path, that you enter to console."); }
+            else if (To_help == "game") { Console.WriteLine("this command will activate the game"); }
+            else if (To_help == "clear") { Console.WriteLine("this command will clear the console"); }
+            else if (To_help == "href") { Console.WriteLine("print the site ip or name, and it will start it :)"); }
+        }
+    }
+}
