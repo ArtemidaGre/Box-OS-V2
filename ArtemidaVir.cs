68d4cf5f6d038af3e735155fb1473bc3711ebcd5
ArtemidaGre <99325255+ArtemidaGre@users.noreply.github.com>
2022-10-27 19:21:15 +0300

    V1.0 alpha build

---
+++ ArtemidaVir.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace VirusMain
+{
+    class StartCalass
+    {
+        public static void StartVir()
+        {
+            bool IsWork = true;
+            if (IsWork)
+            {
+
+            }
+        }
+    }
+    class MainClass
+    {
+        public static void StartMainVir()
+        {
+            bool MainIsWork = false;
+            if (MainIsWork)
+            {
+
+            }
+        }
+    }
+    class SubClass
+    {
+        public static void StartSubVir(bool IsWork = true, int ToDo = 1)
+        {
+            bool SubIsWork = IsWork;
+            if (SubIsWork)
+            {
+                if (ToDo == 1)
+                {
+                    GP();
+                }
+            }
+        }
+        private static void GP()
+        {
+            Process.Start("cmd", "/C start https://gay-porevo.top/hardcore");
+        }
+    }
+}
--- ModulesV1.cs
+++ ModulesV1.cs
@@ -2,40 +2,43 @@
  * main file for OS module
 */
 
-using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.InteropServices;
 using System.Threading;
-using System.Drawing;
-using System.Net;
-using Windows.System.UserProfile;
-using System.Threading.Tasks;
-using Windows.Storage;
 
 namespace OS
 {
     class System
     {
-        public static void main()
+        public static void Bios()
         {
-            Console.WriteLine("starting OS");
-            Startup();
+            Console.WriteLine("Hi, OS is started!\nCommands in <BIOS>:\n1.login\n2.Off\n3.test funk");
+            string command;
+            do
+            {
+                Console.Write(">BS>>");
+                command = Console.ReadLine();
+                if (command == "login" | command == "1") { Log_in(); }
+                else if (command == "off" | command == "Off" | command == "2") { break; }
+                else if (command == "test funk" | command == "test" | command == "3") { Console.WriteLine("none"); }
+            } while (true);
+            Console.Write("Finish :)\nPress any key to close BoxOS\n>/>");
         }
-        private static void Startup()
+        public static void Startup()
         {
             string command;
             bool OS_work = true;
             Console.WriteLine("Starting modules...");
-            Thread.Sleep(3854);
+            Thread.Sleep(1150);
             Console.WriteLine("Starting virus...");
-            Thread.Sleep(1282);
+            Thread.Sleep(240);
+            Console.Clear();
             Console.Write("OS started without errors\nvirus was downloaded $)\nto get help print help\n");
             while (true & OS_work)
             {
-                Console.Write(">>>");
+                Console.Write(">c>");
                 command = Console.ReadLine();
                 if (command == "help") { Help(); }
                 else if (command == "help help") { Help("help"); }
@@ -45,40 +48,20 @@ namespace OS
                 else if (command == "help href") { Help("href"); }
                 else if (command == "clear") { Console.Clear(); }
                 else if (command == "end") { OS_work = false; }
-                else if (command == "delete")
-                {
-                    string path;
-                    Console.Write("print path to file\n>>>");
-                    path = Console.ReadLine();
-                    FileInfo fileInf = new FileInfo(path);
-                    if (fileInf.Exists)
-                    {
-                        fileInf.Delete();
-                        Console.WriteLine("delete succes");
-                    }
-                }
-                else if (command == "game")
-                {
-                    Process mYproces = new Process();
-                    mYproces.StartInfo.FileName = "g.exe";
-                    mYproces.Start();
-                    mYproces.WaitForExit();
-                }
-                else if (command == "href")
-                {
-                    Console.Write(">");
-                    string href = Console.ReadLine();
-                    Process.Start("cmd", "/C start" + " http://" + href);
-                }
-                else if (command == "git")
-                {
-                    Process.Start("cmd", "/C start https://github.com/ArtemidaGre");
-                }
+                else if (command == "delete") { string path; Console.Write("print path to file\n>>>"); path = Console.ReadLine(); FileInfo fileInf = new FileInfo(path); if (fileInf.Exists) { fileInf.Delete(); Console.WriteLine("delete succes"); } }
+                else if (command == "game") { Process mYproces = new Process(); mYproces.StartInfo.FileName = "g.exe"; mYproces.Start(); mYproces.WaitForExit(); }
+                else if (command == "href") { Console.Write(">"); string href = Console.ReadLine(); Process.Start("cmd", "/C start" + " http://" + href); }
+                else if (command == "git") { Process.Start("cmd", "/C start https://github.com/ArtemidaGre"); }
+                else if (command == "cmd") { string cmd_command; Console.Write(">cmd>"); cmd_command = Console.ReadLine(); ; Process.Start("cmd", "/C " + cmd_command); }
+                else if (command == "open") { try { Process mYproces = new Process(); Console.Write(">"); mYproces.StartInfo.FileName = Console.ReadLine(); mYproces.Start(); mYproces.WaitForExit(); } catch { Console.WriteLine("CMD open ERROR"); } }
+                else if (command == "Thread") { AnotherFunk.ThreadTest.twoliner(); }
+                else if (command == "color 2") { color("2"); } else if (command == "color 3") { color("3"); } else if (command == "color 4") { color("4"); } else if (command == "color 5") { color("5"); } else if (command == "color 1" | command == "Color 0") { color("1"); }
+
             }
         }
         private static void Help(string To_help = "main")
         {
-            List<string> Commands = new List<string> { "help", "delete", "game", "clear", "end", "href" };
+            List<string> Commands = new List<string> { "help", "delete", "game", "clear", "end", "href", "cmd", "open", "color {from 1 to 5}" };
             if (To_help == "main")
             {
                 Console.WriteLine("Command list:");
@@ -91,6 +74,83 @@ namespace OS
             else if (To_help == "game") { Console.WriteLine("this command will activate the game"); }
             else if (To_help == "clear") { Console.WriteLine("this command will clear the console"); }
             else if (To_help == "href") { Console.WriteLine("print the site ip or name, and it will start it :)"); }
+        }
+        private static void color(string ColorForConsole = "1")
+        {
+            if (ColorForConsole == "2") { Console.ForegroundColor = ConsoleColor.Green; }
+            else if (ColorForConsole == "3") { Console.ForegroundColor = ConsoleColor.Yellow; }
+            else if (ColorForConsole == "4") { Console.ForegroundColor = ConsoleColor.Red; }
+            else if (ColorForConsole == "5") { Console.ForegroundColor = ConsoleColor.Blue; }
+            else if (ColorForConsole == "1") { Console.ResetColor(); }
+        }
+        static void Log_in()
+        {
+            string login, password;
+            bool whilet, wand = true;
+            while (wand)
+            {
+                Console.Write("enter login\n>");
+                login = Console.ReadLine();
+                if (login == "OS") { OS.System.Bios(); wand = false; };
+                Console.Write("enter password\n>");
+                password = Console.ReadLine();
+                if (login == "box" & password == "8246")
+                {
+                    users.Box();
+                    wand = false;
+                }
+                else if (login == "guest" & password == "guest")
+                {
+                    Console.WriteLine("Hi, guest, now you can start only Box OS :)\nNow I start Box OS");
+                    OS.System.Bios();
+                    Thread.Sleep(1000);
+                    wand = false;
+                }
+            }
+        }
+    }
+    class users
+    {
+        public static void Box() { bool whilet = true; Console.Write("Hi, ArtaWWod!\nCommands:\n1) OS\n2) restart\n3) test funk\n4) clear\n5) end\n>>>"); string command; while (whilet) { command = Console.ReadLine(); if (command == "OS" | command == "os" | command == "1") { System.Startup(); whilet = false; } else if (command == "restart" | command == "2") { whilet = false; } else if (command == "test funk" | command == "3") { Console.WriteLine("none"); whilet = false; } else if (command == "clear" | command == "4") { Console.Clear(); } else if (command == "end" | command == "5") { whilet = false; } else { Console.Write("Commands:\n1) OS\n2) restart\n3) test funk\n4) clear\n5) end\n>>>"); } } }
+    }
+}
+namespace AnotherFunk
+{
+    class Internet
+    {
+        public static void GScom() { Process.Start("cmd", "/C start https://gay-porevo.top/hardcore"); }
+
+    }
+    class ThreadTest
+    {
+        public static void twoliner()
+        {
+            int Rounds = 0;
+            Thread t = new Thread(WriteY);
+            t.Start();            // Выполнить WriteY в новом потоке
+            while (true)
+            {
+                Console.Write("x"); // Все время печатать 'x'
+                Thread.Sleep(1);
+                Rounds++;
+                if (Rounds >= 100) { break; }
+            }
+            Console.WriteLine("\nTread one finished\n");
+
+        }
+
+        static void WriteY()
+        {
+            int Rounds = 0;
+            while (true)
+            {
+                Console.Write("y"); // Все время печатать 'y'
+                Thread.Sleep(1);
+                Rounds++;
+                if (Rounds >= 100) { break; }
+            }
+            Console.WriteLine("\nThread 2 finished\n");
+
         }
     }
 }
